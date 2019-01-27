6a967495f6bddb52ac1ef7cdc2eaa07bd393da82
yuji <yuji38kwmt@yahoo.co.jp>
2019-01-27 14:00:19 +0900

    space keyでtitleシーンに移動

--- Assets/Scripts/GameOverDirector.cs
+++ Assets/Scripts/GameOverDirector.cs
@@ -14,7 +14,7 @@ public class GameOverDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetKeyDown("space")) {
             SceneManager.LoadScene("Title");
         }   
     }
