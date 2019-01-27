4966be57064e471fe22b58518ca9f747db0158fc
yuji <yuji38kwmt@yahoo.co.jp>
2019-01-27 12:51:06 +0900

    Clearシーンを追加。未確認。

---
+++ Assets/Scripts/MyHouseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyHouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D hit)
+    {
+        if (hit.CompareTag ("Player")) {
+            PlayerController playerController = hit.gameObject.GetComponent<PlayerController>();
+            Debug.Log("Playerが家に入った。playerController.hasKey=" + playerController.hasKey);
+            if (playerController.hasKey) {
+                SceneManager.LoadScene("Clear");
+
+            } else {
+                // TODO 何か吹き出しを出す？
+            }
+            
+        }
+    }      
+}
